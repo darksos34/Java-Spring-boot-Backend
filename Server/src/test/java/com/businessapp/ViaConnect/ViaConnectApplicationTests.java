8e57afdcf8569a83661de13f8d5ffe6e5288aa86
Your Name <j.hamwijk34@gmail.com>
2021-02-28 14:05:22 +0100

    Commit Server to Repo

---
+++ Server/src/test/java/com/businessapp/ViaConnect/ViaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessapp.ViaConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
