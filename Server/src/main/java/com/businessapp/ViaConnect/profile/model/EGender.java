034f908f0385eb6ac4aa302a3555c58140708212
Your Name <j.hamwijk34@gmail.com>
2021-02-28 14:03:10 +0100

    remove

--- Server/src/main/java/com/businessapp/ViaConnect/profile/model/EGender.java
+++
@@ -1,7 +0,0 @@
-package com.businessapp.ViaConnect.profile.model;
-
-public enum EGender { // Enumerated gender list (will not change, as such enumerated)
-    MALE,
-    FEMALE,
-    OTHER
-}
--- Server/src/main/java/com/businessapp/ViaConnect/profile/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.businessapp.ViaConnect.profile.repository;
-
-import com.businessapp.ViaConnect.profile.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository // Instruct Spring Boot that this is the Repository
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
