ded0d24d17dea93f4d378f231fdb4ef2370db5ca
Your Name <j.hamwijk34@gmail.com>
2021-03-01 13:56:38 +0100

    Update Maven
    Git ignore was ignoring maven .

---
+++ Server/target/classes/import.sql
@@ -0,0 +1,4 @@
+
+-- INSERT INTO users(created_at, updated_at, email, password, username) VALUES ('2021-05-12 08:00:00', '2021-05-12 08:00:00', 'user@jordycoder.nl', '$2a$10$GckdgpfIJ.NhceNRgh6Aue4AX9fcrmA6mbRRU824UYl8tYAXd3GvG', 'user');
+-- INSERT INTO users(created_at, updated_at, email, password, username) VALUES ('2021-05-12 08:00:00', '2021-05-12 08:00:00', 'moderator@jordycoder.nl', '$2a$10$ozJqsGBy.S9uBPuPIFTYP.qzbbUiF5HPggzZXr/tpwehGuIZuVLae', 'moderator');
+-- INSERT INTO users(created_at, updated_at, email, password, username) VALUES ('2021-05-12 08:00:00', '2021-05-12 08:00:00', 'admin@jordycoder.nl', '$2a$10$CzuliGNQSRoi8IDMe/RUreQWpbmViC.E5qaZjhpAjypgfsOF9afyG', 'admin');
