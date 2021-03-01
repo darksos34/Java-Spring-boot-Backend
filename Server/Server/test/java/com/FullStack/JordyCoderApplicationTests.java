d54d2eb11895e73a971c99a3edcc029b15f6f503
Your Name <j.hamwijk34@gmail.com>
2021-03-01 13:29:05 +0100

    Clean up code

--- Server/Server/test/java/com/FullStack/JordyCoderApplicationTests.java
+++ Server/Server/test/java/com/FullStack/JordyCoderApplicationTests.java
@@ -1,10 +1,10 @@
-package com.businessapp.ViaConnect;
+package com.FullStack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ViaConnectApplicationTests {
+class JordyCoderApplicationTests {
 
 	@Test
 	void contextLoads() {
